889581e1b172870b44ac3983a1f7a6bc3b7950e0
Jose <j.a.ojeda@live.com>
2019-11-21 16:10:29 -0800

    wrote test for verify

--- test/app.test.js
+++ test/app.test.js
@@ -43,4 +43,34 @@ describe('app routes', () => {
     });
   });
 
+  it('errors signin a user when a bad password is provided', async() => {
+    await User.create({ username: 'test', password: '1234' });
+    const res = await request(app)
+      .post('/api/v1/auth/signin')
+      .send({ username: 'me', password: '1234' });
+    expect(res.status).toEqual(401);
+    expect(res.body).toEqual({
+      status: 401,
+      message: 'Invalid username/password'
+    });
+  });
+
+  it('can verify a logged in user', async() => {
+    const agent = request.agent(app);
+    await User.create({ username: 'test', password: '1234' });
+    await agent
+      .post('/api/v1/auth/signin')
+      .send({ username: 'test', password: '1234' });
+
+    return agent
+      .get('/api/v1/auth/verify')
+      .then(res => {
+        expect(res.body).toEqual({
+          _id:expect.any(String),
+          username: 'test'
+        });
+      });
+  });
+
+
 });
